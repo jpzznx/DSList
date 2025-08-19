607fa4c040d0e841e5663305a5cb29fdd1bba547
Jp <jotapk203@gmail.com>
2025-08-18 21:27:13 -0300

    Game repository, service, controller

---
+++ src/main/java/com/projeto/dslist/dto/GameMinDTO.java
@@ -0,0 +1,47 @@
+package com.projeto.dslist.dto;
+
+import org.springframework.stereotype.Repository;
+
+import com.projeto.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private long id;
+	private String title;
+	private int year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	
+	public GameMinDTO() {
+		
+	}
+
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+}
---
+++ src/main/java/com/projeto/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+}
