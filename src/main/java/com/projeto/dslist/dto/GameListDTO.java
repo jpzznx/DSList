a27747a47a9552d66c8478d9d2f79034c7f798ff
Jp <jotapk203@gmail.com>
2025-08-19 20:34:31 -0300

    GET Lists

---
+++ src/main/java/com/projeto/dslist/dto/GameListDTO.java
@@ -0,0 +1,36 @@
+package com.projeto.dslist.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.projeto.dslist.entities.GameList;
+
+public class GameListDTO {
+	private Long id;
+	private String name;
+	
+	public GameListDTO(){
+	}
+
+	public GameListDTO(GameList list) {
+		BeanUtils.copyProperties(list, this);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/projeto/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
